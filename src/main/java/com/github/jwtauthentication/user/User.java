0d7633235fc0358631d53a4c72dae8b4b6469905
Guilherme Costa Sales <guilhermecosales@gmail.com>
2023-01-12 10:44:46 -0300

    feat: implement the user entity

---
+++ src/main/java/com/github/jwtauthentication/user/User.java
@@ -0,0 +1,24 @@
+package com.github.jwtauthentication.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
