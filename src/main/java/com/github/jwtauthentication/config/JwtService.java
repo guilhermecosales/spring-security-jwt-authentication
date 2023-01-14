cdb653c1eec1aaea46477fecaf2043a4242a74e7
Guilherme Costa Sales <guilhermecosales@gmail.com>
2023-01-13 21:42:49 -0300

    fix: change the signature algorithm

--- src/main/java/com/github/jwtauthentication/config/JwtService.java
+++ src/main/java/com/github/jwtauthentication/config/JwtService.java
@@ -38,8 +38,8 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 6 * 24))
-                .signWith(getSignInKey(), SignatureAlgorithm.ES256)
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
